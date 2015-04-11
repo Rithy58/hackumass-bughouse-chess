d73960bfbea4ec88657b02bc6fc07947d79fd5a3
rcui <rcui@umass.edu>
2015-04-11 18:14:23 -0400

    Main.java for testing purposes

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,24 @@
+/**
+ * @author Richard Cui
+ */
 package main;
+
+import javax.swing.UIManager;
+
+import menus.*;
 
 public class Main {
 
+	public static void main(String... args) {
+		for (String string : args) {
+			System.out.println(string + " ");
+		}
+		
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			System.err.println("Exception.\nError code: Richard is not sure whats going on.");
+		}
+		new ServerUI();
+	}
 }

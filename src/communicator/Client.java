9bfe0dff7064a1b74d231549ee977b57a73e6055
rcui <rcui@umass.edu>
2015-04-11 16:08:58 -0400

    started server stuff

---
+++ src/communicator/Client.java
@@ -0,0 +1,24 @@
+package communicator;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Client implements Communicator{
+
+	@Override
+	public void sendObject(Object o) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void close() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

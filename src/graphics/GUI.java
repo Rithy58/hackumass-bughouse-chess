5b497e78a434a7cf9ff5d8f6d6321e1d39aed9cd
dztam <dztam@umass.edu>
2015-04-11 21:23:32 -0400

    Progress with setting up board pieces

--- src/graphics/GUI.java
+++ src/graphics/GUI.java
@@ -6,24 +6,36 @@ import util.Constants;
 
 public class GUI {
 	public JFrame frame;
-	
+
 	/**
 	 * Initialize image icons
 	 */
-	ImageIcon blank = new ImageIcon("blank.png");
-	ImageIcon black_bishop = new ImageIcon(getClass().getResource("black_bishop.png"));
-	ImageIcon black_king = new ImageIcon("black_king.png");
-	ImageIcon black_knight = new ImageIcon("black_knight.png");
-	ImageIcon black_pawn = new ImageIcon("black_pawn.png");
-	ImageIcon black_queen = new ImageIcon("black_queen.png");
-	ImageIcon black_rook = new ImageIcon("black_rook.png");
-	ImageIcon white_bishop = new ImageIcon("white_bishop.png");
-	ImageIcon white_king = new ImageIcon("white_king.png");
-	ImageIcon white_knight = new ImageIcon("white_knight.png");
-	ImageIcon white_pawn = new ImageIcon("white_pawn.png");
-	ImageIcon white_queen = new ImageIcon("white_queen.png");
-	ImageIcon white_rock = new ImageIcon("white_rock.png");
-	
+	ImageIcon blank = new ImageIcon(getClass().getResource("blank.png"));
+	ImageIcon black_bishop = new ImageIcon(getClass().getResource(
+			"black_bishop.png"));
+	ImageIcon black_king = new ImageIcon(getClass().getResource(
+			"black_king.png"));
+	ImageIcon black_knight = new ImageIcon(getClass().getResource(
+			"black_knight.png"));
+	ImageIcon black_pawn = new ImageIcon(getClass().getResource(
+			"black_pawn.png"));
+	ImageIcon black_queen = new ImageIcon(getClass().getResource(
+			"black_queen.png"));
+	ImageIcon black_rook = new ImageIcon(getClass().getResource(
+			"black_rook.png"));
+	ImageIcon white_bishop = new ImageIcon(getClass().getResource(
+			"white_bishop.png"));
+	ImageIcon white_king = new ImageIcon(getClass().getResource(
+			"white_king.png"));
+	ImageIcon white_knight = new ImageIcon(getClass().getResource(
+			"white_knight.png"));
+	ImageIcon white_pawn = new ImageIcon(getClass().getResource(
+			"white_pawn.png"));
+	ImageIcon white_queen = new ImageIcon(getClass().getResource(
+			"white_queen.png"));
+	ImageIcon white_rook = new ImageIcon(getClass().getResource(
+			"white_rook.png"));
+
 	/**
 	 * Create the application.
 	 */
@@ -57,7 +69,15 @@ public class GUI {
 		JButton[][] pieces = new JButton[Constants.BOARD_SIZE][Constants.BOARD_SIZE];
 		for (int i = 0; i < Constants.BOARD_SIZE; i++) {
 			for (int j = 0; j < Constants.BOARD_SIZE; j++) {
-				pieces[i][j] = new JButton();
+				if (i == 0 && j == 0){
+					pieces[i][j] = new JButton(black_rook);
+				}
+				else if (i == 1) {
+					pieces[i][j] = new JButton(black_pawn);
+				} 
+				else {
+					pieces[i][j] = new JButton(blank);
+				}
 				panel.add(pieces[i][j]);
 				pieces[i][j].addActionListener(new onClick(1, i, j));
 				if ((i + j) % 2 == 0) {
@@ -80,10 +100,6 @@ public class GUI {
 			}
 		}
 
-	}
-	
-	private void startGame(){
-		
 	}
 
 }

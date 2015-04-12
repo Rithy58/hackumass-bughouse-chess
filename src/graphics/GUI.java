6e68f31f6c3edd3fae44c9034c90443ed014fbec
dztam <dztam@umass.edu>
2015-04-11 20:13:11 -0400

    Moved GUI main into Main package

--- src/graphics/GUI.java
+++ src/graphics/GUI.java
@@ -6,23 +6,9 @@ import util.Constants;
 
 public class GUI {
 
-	private JFrame frame;
+	public JFrame frame;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					GUI window = new GUI();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+
 
 	/**
 	 * Create the application.
---
+++ src/main/GUIMain.java
@@ -0,0 +1,26 @@
+package main;
+
+import graphics.GUI;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class GUIMain {
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUI window = new GUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
